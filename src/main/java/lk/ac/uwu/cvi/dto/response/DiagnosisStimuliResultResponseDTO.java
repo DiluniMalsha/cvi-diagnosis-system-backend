a41b4dda4cba37c7c9aedc6ef0d12272971a669f
Diluni Malsha <dmalsha@lk.onsystechnologies.com>
2024-05-04 00:52:26 +0530

    Created response DTO for diagnosis stimuli result

---
+++ src/main/java/lk/ac/uwu/cvi/dto/response/DiagnosisStimuliResultResponseDTO.java
@@ -0,0 +1,5 @@
+package lk.ac.uwu.cvi.dto.response;
+
+public record DiagnosisStimuliResultResponseDTO(Long id, Integer frameFrame, Integer stimuliFrameNumber,
+                                                Integer resultFrameNumber) {
+}
