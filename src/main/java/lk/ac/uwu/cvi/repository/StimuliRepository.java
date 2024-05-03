37e3a011440be94048ee128935803d9bd8cfe190
Diluni Malsha <dmalsha@lk.onsystechnologies.com>
2024-05-04 00:28:29 +0530

    Optimized imports

--- src/main/java/lk/ac/uwu/cvi/repository/StimuliRepository.java
+++ src/main/java/lk/ac/uwu/cvi/repository/StimuliRepository.java
@@ -1,6 +1,5 @@
 package lk.ac.uwu.cvi.repository;
 
-import lk.ac.uwu.cvi.entity.Patient;
 import lk.ac.uwu.cvi.entity.Stimuli;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
